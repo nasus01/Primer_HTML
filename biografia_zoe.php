28c931eafe429d05255d92f3c29e965c8d5e611a
christian <christianjack01@hotmail.com>
2019-11-17 13:06:01 -0500

    primera pagina web

---
+++ biografia_zoe.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title> Biografia de zoe</title>
+    <link rel="stylesheet" type="text/css" href="ccs/estilos.css">
+</head>
+<body>
+    <h1>Biografia de zoe</h1>
+    <p>
+        
+
+ZOE
+EL ASPECTO DEL CREPÚSCULO
+
+ZOE
+EL ASPECTO DEL CREPÚSCULO
+Zoe, la mensajera cósmica de Targón, es la personificación de las travesuras, la imaginación y el cambio y, como su heraldo, anuncia grandes acontecimientos que pueden alterar a mundos enteros. Su mera presencia distorsiona las matemáticas arcanas que rigen las realidades; en ocasiones, sin ser consciente de su poder, provoca todo tipo de cataclismos. Tal vez eso explique la actitud indiferente con la cual Zoe "cumple" con su deber. Tomándose todo el tiempo de los mundos para jugar, engañar a los mortales o simplemente entretenerse a sí misma. Un encuentro con Zoe puede ser alegre y alentador, pero siempre hay algo más detrás de las apariencias y a menudo es muy peligroso.
+
+Como corresponde a la naturaleza de su aspecto targoniano, Zoe no ascendió al poder de una manera tradicional. No lo logró tras resultar victoriosa ante las peores adversidades, tampoco se sacrificó por una noble causa ni superó la prueba existencial de escalar el Monte Targón. Zoe era una chica normal que a primera vista fue elegida al azar.
+
+Sus maestros Lunari señalaron que Zoe era una chica creativa, pero también obstinada, floja, muy distraída y traviesa. Un día, mientras se escapaba de sus estudios de la magia sagrada, buscando hacer algo ''menos aburrido'', el Aspecto del Crepúsculo targoniano se fijó en ella.
+    </P>
+
+ <img  id="imagen2" src="Zoe_loading_screen.png">
+ <br>
+ <a href="index.php"> volver</a> 
+
+
+
+
+</body>
+
+
+
+
+</html>
---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE  html>
+<html>
+<head>
+<title>
+ hola mundo!
+</title>
+<link rel="stylesheet" type="text/css" href="ccs/estilos.css">
+</head>
+<body>
+    <!-- esto es un comentario -->
+    <h1>hola mundo, me llamo christian!</h1>
+    <h2>subtitulo</h2>
+    <h3>contenido de informacion</h3>
+    <h4>un texto pequeño </h4>
+    <h5>un texto mas pequeño</h5>
+    <h6>un texto muy pequeño</h6>
+    <p>esto es un parrafo  para colocar mucha informacion</p>
+    <p>tegon un texto con estilo <b>negrita</b> </p>
+    <a href="http://www.google.com.co" target="_blank">click aqui para ir a google</a>
+    <br>
+    hola mundo 2.
+    <br>
+    este es otro texto
+    <br>
+<a href="biografia_zoe.php"><img id="imagen1"src="Zoe_9.jpg"></a>
+</body>
+</html>
